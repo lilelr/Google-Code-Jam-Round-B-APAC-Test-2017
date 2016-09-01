107e412dd7066d3642a0d8d0ecc215b2dcf1bb40
lilelr <lilelr@163.com>
2016-09-01 20:06:15 +0800

    Initial Commit

---
+++ gcj Round B APAC Test 2017/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gcj Round B APAC Test 2017
+//
+//  Created by YuXiao on 9/1/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
