7ba84aea4b9de3d262f0430bd0763ad478a1908a
lilelr <lilelr@163.com>
2016-09-01 20:35:38 +0800

    1

--- gcj Round B APAC Test 2017/main.cpp
+++ gcj Round B APAC Test 2017/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  gcj Round B APAC Test 2017
-//
-//  Created by YuXiao on 9/1/16.
-//  Copyright © 2016 YuXiao. All rights reserved.
-//
 
 #include <iostream>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+using namespace std;
+#define LL long long
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.in","r",stdin);
+    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.out","w",stdout);
+    int T,t;
+    cin>>T;
+    t=1;
+    LL L,R;
+    LL ans;
+    while (T--){
+        cin>>L;
+        cin>>R;
+        LL pairs = min(L,R);
+        if (pairs==0) {
+            ans=0;
+        }else{
+            ans=pairs*(pairs+1)/2;
+        }
+        printf("Case #%d: %lld\n",t++,ans);
+    }
     return 0;
 }
