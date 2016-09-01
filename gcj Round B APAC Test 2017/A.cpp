01f530e928bd7560945d3071b764581b1897bf76
lilelr <lilelr@163.com>
2016-09-01 23:28:59 +0800

    2 快速幂取余算法

---
+++ gcj Round B APAC Test 2017/A.cpp
@@ -0,0 +1,34 @@
+//
+//#include <iostream>
+//#include <cstdio>
+//#include <string>
+//#include <algorithm>
+//#include <cmath>
+//#include <vector>
+//#include <map>
+//#include <set>
+//#include <queue>
+//using namespace std;
+//#define LL long long
+//
+//int main() {
+//    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.in","r",stdin);
+//    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.out","w",stdout);
+//    int T,t;
+//    cin>>T;
+//    t=1;
+//    LL L,R;
+//    LL ans;
+//    while (T--){
+//        cin>>L;
+//        cin>>R;
+//        LL pairs = min(L,R);
+//        if (pairs==0) {
+//            ans=0;
+//        }else{
+//            ans=pairs*(pairs+1)/2;
+//        }
+//        printf("Case #%d: %lld\n",t++,ans);
+//    }
+//    return 0;
+//}
--- gcj Round B APAC Test 2017/main.cpp
+++ gcj Round B APAC Test 2017/main.cpp
@@ -10,24 +10,48 @@
 #include <queue>
 using namespace std;
 #define LL long long
+const LL mod=1e9+7;
+
+long long  modExp(long long  a,long long  b,long long  n){
+    long long  t,y;
+    t = 1; y = a;
+    while(b){
+        if(b % 2) t = t * y % n;
+        y = y * y % n;
+        b >>= 1;
+    }
+//    printf("%lld\n",t);
+    return t;
+}
 
 int main() {
-    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.in","r",stdin);
-    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/A-large-practice.out","w",stdout);
+//    modExp(2, 6, 63);
+    
+    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/B-small-practice.in","r",stdin);
+    freopen("/Users/yuxiao/XcodeProject/gcj Round B APAC Test 2017/gcj Round B APAC Test 2017/B-pratice.out","w",stdout);
     int T,t;
     cin>>T;
     t=1;
-    LL L,R;
+    LL N;
+    LL A,B,K;
     LL ans;
+//    cout<<mod<<endl;
     while (T--){
-        cin>>L;
-        cin>>R;
-        LL pairs = min(L,R);
-        if (pairs==0) {
-            ans=0;
-        }else{
-            ans=pairs*(pairs+1)/2;
+       
+        cin>>A>>B>>N>>K;
+        ans=0;
+        for (LL i=1; i<=N; i++) {
+            for(LL j=1;j<=N;j++){
+                if(i!=j){
+                    LL left = modExp(i, A, K);
+                    LL right = modExp(j, B, K);
+                    if ((left %K+right %K)%K == 0) {
+                        ans = (ans+1)%mod;
+                    }
+                }
+            }
         }
+        
         printf("Case #%d: %lld\n",t++,ans);
     }
     return 0;
